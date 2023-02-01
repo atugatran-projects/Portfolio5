557123f94c558734986979976b300f285ad1356d
Atugatran <atugatran@gmail.com>
2023-02-02 03:44:07 +0530

    Improve to portfolio5

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Light Mode && Dark Mode
 const input = document.querySelector(".theme-switcher input");
 input.addEventListener("change", (e) => {
   if (e.target.checked) {
@@ -7,3 +7,14 @@ input.addEventListener("change", (e) => {
     document.body.setAttribute("data-theme", "light");
   }
 });
+
+// Typedjs
+var typed = new Typed('.type', {
+  strings: ["Atul Goyal" , "FullStack Developer"],
+  typeSpeed: 40,
+  backSpeed: 40,
+  cursorChar: '_',
+  shuffle: true,
+  smartBackspace: false,
+  loop: true
+  });
